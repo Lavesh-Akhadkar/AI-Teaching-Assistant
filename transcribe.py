f8e20633070f056d16879d2a9b58dd1568805aa2
Lavesh Akhadkar <117550850+Lavesh-Akhadkar@users.noreply.github.com>
2024-12-28 03:28:19 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,27 +1,13 @@
-import whisper
-import json
+from swarmauri.llms.concrete.GroqAIAudio import GroqAIAudio
 import os
+from dotenv import load_dotenv
 
-def tts():
-
-    audio = whisper.load_audio("audio/audio.mp3")
-
-    model = whisper.load_model("tiny", device="cpu")
-    result = whisper.transcribe(model, audio, language="en")
-
-    #if transcript folder doesn't exist, create it
-    try:
-        os.mkdir("transcript")
-    except:
-        pass
-
-    with open("transcript/transcript.json", "w") as f:
-        f.write(json.dumps(result, indent=2, ensure_ascii=False))
+load_dotenv()
 
 
-def get_transcript():
-    with open("transcript/transcript.json", "r") as f:
-        data = json.load(f)
-    transcript = data["text"]
-    return transcript
+def transcribe_audio(audio_path="audio/audio.mp3"):
+    llm = GroqAIAudio(api_key=os.environ.get("GROQ_API_KEY"))
 
+    text = llm.predict(audio_path, task="transcription")
+
+    return text
